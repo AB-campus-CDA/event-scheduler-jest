01b3d44d44870a775c23ebb677426f83d7cc4039
AurelBouchard <au.bouchard@gmail.com>
2024-02-16 15:09:38 +0100

    ven. 16 févr. 2024 15:09:38 CET

--- test/serializer.test.js
+++ test/serializer.test.js
@@ -6,7 +6,7 @@ describe("Event Serializer",()=> {
 
     test('Test event serializer', async () => {
         let eventSerializer = new EventSerializer();
-        let event = new Event(new Date('2019-12-17T03:24:00'), new Date('2019-12-17T13:24:00'), "Hello World", "Campus Numerique", "This is an hello world");
+        let event = new Event(new Date('2019-12-17T03:24:00.000+01:00'), new Date('2019-12-17T13:24:00.000+01:00'), "Hello World", "Campus Numerique", "This is an hello world");
         expect(eventSerializer.serialize(event)).toBe('{"startTime":"2019-12-17T02:24:00.000Z","endTime":"2019-12-17T12:24:00.000Z","title":"Hello World","location":"Campus Numerique","description":"This is an hello world"}');
     })
 
